37608eed50dfe17e5991b59b92952fdca2e42c4a
Lucas Zenichi Terada <lzenichi@protonmail.com>
2021-08-04 18:07:24 -0300

    improve the main file

--- dersimulator.py
+++ dersimulator.py
@@ -1,10 +1,33 @@
-from source import powerflow
-import os, sys
+from source.Processing import measurements
+from source.Processing import structures
+from source.LoadFlow import powerflow
 import json
+import os
+
 
 
 config = json.load(open("config.json"))
-feeder = config["feeders"][0]
+feeder = config['DSS']["feeders"][0]
 
 Circuit = powerflow.run(feeder)
+
+# Define structures
+bus = structures.bus(Circuit)
+elements = structures.elements(Circuit)
+
+# Get measures
+bus = measurements.busdata(Circuit, bus, Circuit.AllBusNames)
+elements = measurements.elementsdata(Circuit, elements, Circuit.AllElementNames)
+
+if not os.path.exists('Estudos/'):
+    os.makedirs('Estudos/')
+
+# Save files
+with open('Estudos/bus.json', 'w') as f:
+    json.dump(bus,f, indent=2)
+
+with open('Estudos/elements.json', 'w') as f:
+    json.dump(elements, f, indent=2)
+
 a = 1
+
